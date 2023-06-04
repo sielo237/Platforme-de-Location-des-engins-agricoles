abcbe94d0540b330f76d7a662b5de52313700c50
sielo237 <69084721+sielo237@users.noreply.github.com>
2023-06-04 16:43:38 +0200

    update authentification

--- Api/middlewares/authentification.js
+++ Api/middlewares/authentification.js
@@ -20,8 +20,8 @@ const authentification = async (req, res, next) => {
 
     if (!user) throw new Error();
     
-    req.authToken = authToken;
-    req.user = user;
+    req.authToken = authToken; // on accède au token actuel de l'utilisateur
+    req.user = user; // on stocke les infos de l'utilisateur  pour pouvoir utiliser sur une nouvelle route
     next();
   } catch (error) {
     console.log(error);
@@ -30,36 +30,3 @@ const authentification = async (req, res, next) => {
 };
 
 module.exports = authentification;
-
-
-/*const jwt=require('jsonwebtoken');
-const User=require('../models/user');  
-require('dotenv').config();
-
-
-
-// ce middleware permet à l'utilisateur de s'authentifier pour pouvoir accéder à une page
-const authentification = async(req,res,next)=>{
-    try {
-        const authToken=req.header('Authorization').replace('Bearer ','');
-        console.log(authToken);
-        const decodedToken=jwt.verify(authToken, process.env.CLE);
-        console.log(process.env.CLE);
-        const user=await User.findOne({_id: decodedToken._id, 'authTokens.authToken':authToken}); //trouver l'id du client  correspondant en bd et verifi si le token existe
-        
-        if(!user) throw new Error();
-
-        req.authToken= authToken ;// on accède au token actuel de l'utilisateur
-        req.user=user; // on stocke les infos de l'utilisateur  pour pouvoir utiliser sur une nouvelle route
-       next(); 
-    } catch (error) {
-        console.log(error);
-        res.status(401).send('veuillez vous authentififié')
-    }
-
-
-}
-
-
-module.exports=authentification;
-*/
